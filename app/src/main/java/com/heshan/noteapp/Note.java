87d995b0d8d53d2bda3154140107d2b783c0d738
Heshan <heshan.imedha@gmail.com>
2021-10-09 22:30:11 +0530

    Note Class Added

--- app/src/main/java/com/heshan/noteapp/Note.java
+++ app/src/main/java/com/heshan/noteapp/Note.java
@@ -1,4 +1,44 @@
 package com.heshan.noteapp;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// Marks a class as an entity. This class will have a mapping SQLite table in the database.
+@Entity(tableName = "note_table")
 public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private  int id;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
