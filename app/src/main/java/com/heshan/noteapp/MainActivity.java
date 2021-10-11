dbd08531f2a4a77807c11b63e31d8156efdb6c29
Heshan <heshan.imedha@gmail.com>
2021-10-11 15:03:42 +0530

    MainActivity Class Updated

--- app/src/main/java/com/heshan/noteapp/MainActivity.java
+++ app/src/main/java/com/heshan/noteapp/MainActivity.java
@@ -1,18 +1,28 @@
 package com.heshan.noteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+
+            }
+        });
+
     }
 }
