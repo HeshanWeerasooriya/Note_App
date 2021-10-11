dfd62867b0ca864878eae54a1eb12b2cdc149019
Heshan <heshan.imedha@gmail.com>
2021-10-11 13:38:54 +0530

    NoteViewModel Class Added

--- app/src/main/java/com/heshan/noteapp/MainActivity.java
+++ app/src/main/java/com/heshan/noteapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.heshan.noteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
